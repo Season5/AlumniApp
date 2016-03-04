ed28075963e7e14dda33d987650158b431dc2117
Elvisthacoder <ekk213@gmail.com>
2016-03-04 19:59:58 +0300

    Add changes

--- app/src/main/java/com/example/jaykayitare/alumniapp/Cohort2.java
+++ app/src/main/java/com/example/jaykayitare/alumniapp/Cohort2.java
@@ -8,6 +8,6 @@ public class Cohort2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cohoort2);
+        setContentView(R.layout.cohort2);
     }
 }
