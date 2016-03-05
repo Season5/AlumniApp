396898b1ffbfb73467ae7eac7350476496e95ad8
jmvkayitare <jmvkayitare@yahoo.com>
2016-03-06 02:07:51 +0300

    Add feature

---
+++ app/src/main/java/com/example/jaykayitare/alumniapp/Cohort3.java
@@ -0,0 +1,21 @@
+package com.example.jaykayitare.alumniapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Cohort3 extends AppCompatActivity {
+    String[] names = {"Diana Omondi","James Omondi","Ian Omondi","John Omondi","Diana Omondi","James Omondi","Ian Omondi","John Omondi"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cohort3);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.listview, names);
+
+        ListView listView = (ListView) findViewById(R.id.listView2);
+        listView.setAdapter(adapter);
+
+    }
+}
